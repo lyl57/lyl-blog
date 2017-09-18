e075b7c3e0fae698cf4e85c19a62d8ea73f7edf3
liaoyalang <liaoyalang@itouchtv.cn>
2017-09-18 15:13:31 +0800

    api、mng、web service commit

---
+++ blog-web/src/main/java/com/lyl57/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.lyl57.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by lyl57 on 2017/3/13.
+ */
+@RestController
+@RequestMapping("/hello1")
+public class HelloController {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("/")
+    public String test1() {
+        log.info("test");
+        return "web:Hello World";
+    }
+
+
+}
