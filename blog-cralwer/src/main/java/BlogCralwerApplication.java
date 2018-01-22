0df72302a57dfbba665b556e7168add45066b8b5
liaoyalang <liaoyalang@itouchtv.cn>
2018-01-22 17:09:05 +0800

    test

---
+++ blog-cralwer/src/main/java/BlogCralwerApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Created by lyl57 on 2018/1/22
+ */
+@SpringBootApplication
+public class BlogCralwerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogCralwerApplication.class, args);
+    }
+}
